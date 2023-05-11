47a5caa8bcc488d33818f58a6f330bd3f1655261
EL Houari <bouchra.el-houari@alumni.univ-avignon.fr>
2023-05-12 00:26:46 +0200

    Ajout page profil

---
+++ src/application/Main.java
@@ -0,0 +1,38 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {/*
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,1000,600);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Profil");
+			primaryStage.show();
+			*/
+            //AnchorPane root = (AnchorPane) FXMLLoader.load(Main.class.getResource("/application/Profil.fxml"));
+            Parent root  = FXMLLoader.load(getClass().getResource("/application/Profil.fxml"));
+            Scene scene = new Scene(root,1220,700);
+            //scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Profil");
+            primaryStage.setResizable(true);
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
